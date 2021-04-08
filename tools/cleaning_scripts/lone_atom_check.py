d1e47a2411fedb544b52eddb9551eea10e6b301b
Andrew S. Rosen <rosen@u.northwestern.edu>
2021-04-08 18:34:06 -0500

    Update lone_atom_check.py
    Former-commit-id: 841ccc5985b41f26572b58b901f3afaf469492ba

--- tools/cleaning_scripts/lone_atom_check.py
+++ tools/cleaning_scripts/lone_atom_check.py
@@ -6,6 +6,7 @@ import os
 folder = 'path/to/CIFs'
 cifs = os.listdir(folder)
 cifs.sort()
+bad_list = []
 for cif in cifs:
 	mof = Structure.from_file(os.path.join(folder, cif))
 	nn = local_env.CrystalNN()
@@ -14,4 +15,9 @@ for cif in cifs:
 		nbr = graph.get_connected_sites(j)
 		if not nbr:
 			print('Lone atom issue:' + cif+'\n')
+			bad_list.append(cif)
 			break
+
+with open('bad_cifs_lone_atom_check.gcd','w') as w:
+	for bad_cif in bad_list:
+		w.write(bad_cif+'\n')
