0cf4eee6f8df0534ee50b31c8dff245c910da1d9
Andrew S. Rosen <rosen@u.northwestern.edu>
2021-04-08 18:33:24 -0500

    Update check_dist.py
    Former-commit-id: f2529fc8664464763752e06880395e808af34069

--- tools/cleaning_scripts/check_dist.py
+++ tools/cleaning_scripts/check_dist.py
@@ -4,6 +4,7 @@ import numpy as np
 
 cutoff = 0.75  # interatomic distance threshold
 folder = 'path/to/CIFs'
+bad_list = []
 for cif in os.listdir(folder):
 	mof = read(os.path.join(folder, cif))
 	d = mof.get_all_distances()
@@ -11,4 +12,9 @@ for cif in os.listdir(folder):
 	for entry in upper_diag:
 		if entry < cutoff:
 			print('Interatomic distance issue:' + cif.split('.')[0])
+			bad_list.append(cif)
 			break
+
+with open('bad_cifs_distance_check.gcd','w') as w:
+	for bad_cif in bad_list:
+		w.write(bad_cif+'\n')
