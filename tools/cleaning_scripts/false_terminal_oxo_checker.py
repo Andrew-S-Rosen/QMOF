4b529776dd5aa3fc48d559ce46cbc18db3ad297a
Andrew S. Rosen <rosen@u.northwestern.edu>
2021-04-08 21:27:35 -0500

    Update false_terminal_oxo_checker.py
    Former-commit-id: 0552e2100ce3ad8ecad3948c1976b919dd4341c6

--- tools/cleaning_scripts/false_terminal_oxo_checker.py
+++ tools/cleaning_scripts/false_terminal_oxo_checker.py
@@ -80,6 +80,6 @@ for cif in cifs:
 		if bad:
 			break
 
-with open('bad_cifs_oxo_check.gcd','w') as w:
+with open('bad_cifs_oxo_check.txt','w') as w:
 	for bad_cif in bad_list:
 		w.write(bad_cif + '\n')
