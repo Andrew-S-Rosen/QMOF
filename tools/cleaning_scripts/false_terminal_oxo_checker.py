e15a963a22149964b75d7a98dddf1f5d25c55a1a
Andrew S. Rosen <rosen@u.northwestern.edu>
2021-04-08 19:08:11 -0500

    Update false_terminal_oxo_checker.py
    Former-commit-id: 53a30eb3e5563685b68ccd6e26ea256dc0418133

--- tools/cleaning_scripts/false_terminal_oxo_checker.py
+++ tools/cleaning_scripts/false_terminal_oxo_checker.py
@@ -4,21 +4,21 @@ import os
 import numpy as np
 import warnings
 
-# Path to CIFs
-p = r'/path/to/cifs'
-
 # Metals that should not have terminal oxo ligands
-metals = ['Li', 'Na', 'K', 'Rb', 'Cs', 'Fr',
-          'Be', 'Mg', 'Ca', 'Sr', 'Ba', 'Ra',
-          'Sc', 'Y', 'La', 'Ac',
-          'Ti', 'Zr', 'Hf',
-          'Mn',
-          'Fe',
-          'Co',
-          'Ni',
-          'Cu', 'Ag',
-          'Zn', 'Cd',
-          'Al', 'Ga', 'In', 'Tl']
+metals = ['Li','Na','K','Rb','Cs','Fr',
+	'Be','Mg','Ca','Sr','Ba','Ra',
+	'Sc','Y','La','Ac',
+	'Ti','Zr','Hf',
+	'Mn',
+	'Fe',
+	'Co',
+	'Ni',
+	'Cu','Ag',
+	'Zn','Cd',
+	'Al','Ga','In','Tl']
+
+# Path to CIFs
+p = r'/path/to/CIFs'
 
 # Get CIFs from folder
 cifs = os.listdir(p)
@@ -29,58 +29,57 @@ cifs.sort()
 bad_list = []
 for cif in cifs:
 
-    bad = False
+	bad = False
 
-    # Read in CIF, ignoring ASE warnings
-    with warnings.catch_warnings():
-        warnings.simplefilter('ignore')
-        structure = read(os.path.join(p, cif))
+	# Read in CIF, ignoring ASE warnings
+	with warnings.catch_warnings():
+		warnings.simplefilter('ignore')
+		structure = read(os.path.join(p,cif))
 
-    # Get list of atomic symbols
-    syms = np.array(structure.get_chemical_symbols())
+	# Get list of atomic symbols
+	syms = np.array(structure.get_chemical_symbols())
 
-    # Is one of the specified metals in this MOF
-    in_metal_list = False
-    for sym in syms:
-        if sym in metals:
-            in_metal_list = True
-            break
-    if not in_metal_list:
-        continue
+	# Is one of the specified metals in this MOF
+	if not any(item in syms for item in metals):
+		continue
 
-    # Generate neighbor list
-    cutoff = neighborlist.natural_cutoffs(structure)
-    nl = neighborlist.NeighborList(
-        cutoff, self_interaction=False, bothways=True)
-    nl.update(structure)
+	# Initialize neighbor list
+	cutoff = neighborlist.natural_cutoffs(structure)
+	nl = neighborlist.NeighborList(cutoff,self_interaction=False,bothways=True)
+	nl.update(structure)
 
-    # Detect terminal oxo site
-    for i, sym in enumerate(syms):
+	# For every site, check if it is a terminal metal-oxo
+	for i, sym in enumerate(syms):
 
-        # If the site is a specified metal...
-        if sym in metals:
+		# Confirm site is in pre-specified metal list
+		if sym not in metals:
+			continue
 
-            # Get neighbors to metal
-            bonded_atom_indices = nl.get_neighbors(i)[0]
-            if bonded_atom_indices is None:
-                continue
-            bonded_atom_symbols = syms[bonded_atom_indices]
+		# Get neighbors to metal
+		bonded_atom_indices = nl.get_neighbors(i)[0]
+		if bonded_atom_indices is None:
+			continue
+		bonded_atom_symbols = syms[bonded_atom_indices]
 
-            # Find bound O atoms
-            for j, bonded_atom_symbol in enumerate(bonded_atom_symbols):
+		# For every neighbor, check if it's a terminal oxo
+		for j, bonded_atom_symbol in enumerate(bonded_atom_symbols):
 
-                # Determine if it is a terminal oxo
-                if bonded_atom_symbol == 'O':
-                    cn = len(nl.get_neighbors(bonded_atom_indices[j])[0])
-                    if cn == 1:
-                        bad = True
-                        print('Missing H on terminal oxo:' + cif+'\n')
-                        bad_list.append(cif)
-                if bad:
-                    break
-        if bad:
-            break
+			# Confirm neighbor is an O atom
+			if bonded_atom_symbol != 'O':
+				continue
+
+			# Check if the O atom is only bound to the metal
+			cn = len(nl.get_neighbors(bonded_atom_indices[j])[0])
+			if cn == 1:
+				bad = True
+				print('Missing H on terminal oxo: ' + cif)
+				bad_list.append(cif)
+
+			if bad:
+				break
+		if bad:
+			break
 
 with open('bad_cifs_oxo_check.gcd','w') as w:
 	for bad_cif in bad_list:
-		w.write(bad_cif+'\n')
+		w.write(bad_cif + '\n')
