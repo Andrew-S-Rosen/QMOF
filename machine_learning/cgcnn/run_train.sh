5e0d4a85d233dbe26e8f866aab552de015a92cab
Andrew S. Rosen <rosen@u.northwestern.edu>
2021-06-10 02:34:10 -0400

    Update run_train.sh
    Former-commit-id: b2aaec47a57e946d44f3dca143afe8cc02979cdd

--- machine_learning/cgcnn/run_train.sh
+++ machine_learning/cgcnn/run_train.sh
@@ -1,1 +1,1 @@
-python main.py --batch-size 16 --n-conv 5 --n-h 1 --train-ratio 0.8 --val-ratio 0.1 --test-ratio 0.1 --workers 4 --epochs 400 --print-freq 1 data/QMOF-42349 > cgcnn_train.out
+python main.py --batch-size 16 --n-conv 5 --n-h 1 --train-ratio 0.8 --val-ratio 0.1 --test-ratio 0.1 --workers 4 --epochs 400 --print-freq 1 data/QMOF > cgcnn_train.out
