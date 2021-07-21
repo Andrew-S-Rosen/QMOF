49387ddf14ae36c29fcf2b75bcc244a8528d8728
Andrew S. Rosen <rosen@u.northwestern.edu>
2021-07-21 16:02:31 -0500

    Update make_primitive.py
    Former-commit-id: 8f090717e4845d636ef2d271cd3761068d2fe7ae

--- database_tools/make_primitive.py
+++ database_tools/make_primitive.py
@@ -1,7 +1,7 @@
 from pymatgen.core import Structure
 import os
 
-folder = r'path/to/CIFs'
+folder = r'/path/to/CIFs'
 for entry in os.listdir(folder):
 	structure = Structure.from_file(os.path.join(folder,entry),primitive=True)
 	structure.to(filename=os.path.join(folder,entry))
